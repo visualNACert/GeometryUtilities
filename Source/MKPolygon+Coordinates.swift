f664f17f5c976ffc0cfdff691498c5dde5f3119a
Lluís Ulzurrun de Asanza i Sàez <lulzurrun@visualnacert.com>
2016-11-03 12:42:42 +0100

    Added `contains(pointAtCoordinates)` method to `MKPolygon`

--- Source/MKPolygon+Coordinates.swift
+++ Source/MKPolygon+Coordinates.swift
@@ -29,4 +29,71 @@ extension MKPolygon {
 		self.init(coordinates: &copy, count: copy.count)
 	}
 
+	/**
+     Returns whether point at given coordinates is contained in this polygon
+     or not.
+     
+     - parameter coordinates: Coordinates of point to be checked.
+     
+     - returns: `true` if point is contained in this polygon.
+     */
+	public func contains(
+		pointAtCoordinates coordinates: CLLocationCoordinate2D
+	) -> Bool {
+
+		let polygonRenderer = MKPolygonRenderer(polygon: self)
+		let currentMapPoint = MKMapPointForCoordinate(coordinates)
+		let polygonViewPoint = polygonRenderer.pointForMapPoint(currentMapPoint)
+
+		return CGPathContainsPoint(
+			polygonRenderer.path,
+			nil,
+			polygonViewPoint,
+			true
+		)
+
+	}
+
+	/* Possible alternative solution that should be considered...
+	 // - note: Taken from: http://alienryderflex.com/polygon/
+	 public func contains(
+	 pointAtCoordinates coordinates: CLLocationCoordinate2D
+	 ) -> Bool {
+
+	 let corners = self.coordinates()
+
+	 let x = coordinates.longitude
+	 let y = coordinates.latitude
+
+	 let polyX = corners.map { $0.longitude }
+	 let polyY = corners.map { $0.latitude }
+
+	 var i = 0
+	 var j = corners.count - 1
+	 var oddNodes = false
+
+	 while i < corners.count {
+
+	 // If clause prevents division by zero...
+	 if (
+	 (polyY[i] < y && polyY[j] >= y || polyY[j] < y && polyY[i] >= y) &&
+	 (polyX[i] <= x || polyX[j] <= x)
+	 ) {
+	 oddNodes ^= (
+	 polyX[i] +
+	 (y - polyY[i]) / (polyY[j] - polyY[i]) * (polyX[j] - polyX[i])
+	 < x
+	 )
+	 }
+
+	 j = i
+	 i++
+
+	 }
+
+	 return oddNodes
+
+	 }
+	 */
+
 }
