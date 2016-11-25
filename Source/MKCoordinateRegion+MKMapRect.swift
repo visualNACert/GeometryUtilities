a1af9b4e7b8653ddc56f24ed5d3c48ac57706d49
Lluís Ulzurrun de Asanza i Sàez <me@llu.is>
2016-11-25 13:46:49 +0100

    Added `mapRect` attribute

---
+++ Source/MKCoordinateRegion+MKMapRect.swift
@@ -0,0 +1,40 @@
+//
+//  MKCoordinateRegion+MKMapRect.swift
+//  Pods
+//
+//  Created by Lluís Ulzurrun de Asanza Sàez on 25/11/16.
+//
+//
+
+import MapKit
+
+extension MKCoordinateRegion {
+    
+    /// Map rect equivalent to this coordinte region.
+    public var mapRect: MKMapRect {
+        get {
+            
+            let origin = CLLocationCoordinate2D(
+                latitude: self.center.latitude - self.span.latitudeDelta / 2.0,
+                longitude: self.center.longitude - self.span.longitudeDelta / 2.0
+            )
+            
+            let end = CLLocationCoordinate2D(
+                latitude: self.center.latitude + self.span.latitudeDelta / 2.0,
+                longitude: self.center.longitude + self.span.longitudeDelta / 2.0
+            )
+            
+            let originPoint = MKMapPointForCoordinate(origin)
+            let endPoint = MKMapPointForCoordinate(end)
+            
+            return MKMapRectMake(
+                originPoint.x,
+                originPoint.y,
+                endPoint.x - originPoint.x,
+                endPoint.y - originPoint.y
+            )
+            
+        }
+    }
+    
+}
